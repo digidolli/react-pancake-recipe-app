c089b455f74f05c97b835aeb914c9512ca6c9cbd
Digidolli <iamsadekaye@gmail.com>
2022-03-01 12:52:14 +0000

    Completed dynamic active class switch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import RecipeList from './RecipeList.js';
+import {sweetPancakes, savoryPancakes, experimentalPancakes} from "./data.js"
 
 export default function App() {
   const [selected, setSelected] = useState("sweet")
+  const [data, setData] = useState ([]);
   const list = [
   {
     id: "sweet",
@@ -19,6 +21,22 @@ export default function App() {
 }
 ];
 
+useEffect (() => {
+  switch (selected) {
+    case "sweet":
+      setData(sweetPancakes);
+      break;
+    case "savory":
+      setData(savoryPancakes);
+      break;
+    case "experimental":
+      setData(experimentalPancakes);
+      break;
+    default:
+      setData(sweetPancakes)
+  }
+},[selected]);
+
   return (
     <div className="App">
       <h1>Happy Pancake Day</h1>
@@ -30,23 +48,14 @@ export default function App() {
         ))}
       </ul>
       <div className='container'> 
-      <div className='item'>
-      <img src="https://media.istockphoto.com/photos/pancakes-picture-id96430985?k=20&m=96430985&s=612x612&w=0&h=lgPzugJRc70yM9EP4NV0DoNaDJQuQ1bJ-x5bjpDmLI4=" />
-      <h3>Chocolate</h3>
-      </div>
-      <div className='item'>
-      <img src="https://media.istockphoto.com/photos/pancakes-picture-id96430985?k=20&m=96430985&s=612x612&w=0&h=lgPzugJRc70yM9EP4NV0DoNaDJQuQ1bJ-x5bjpDmLI4=" />
-      <h3>Chocolate</h3>
-      </div>
-      <div className='item'>
-      <img src="https://media.istockphoto.com/photos/pancakes-picture-id96430985?k=20&m=96430985&s=612x612&w=0&h=lgPzugJRc70yM9EP4NV0DoNaDJQuQ1bJ-x5bjpDmLI4=" />
-      <h3>Chocolate</h3>
-      </div>
-      <div className='item'>
-      <img src="https://media.istockphoto.com/photos/pancakes-picture-id96430985?k=20&m=96430985&s=612x612&w=0&h=lgPzugJRc70yM9EP4NV0DoNaDJQuQ1bJ-x5bjpDmLI4=" />
-      <h3>Chocolate</h3>
-      </div>
-      </div>
+      {data.map((d) => (
+        <div className='item'>
+          <img scr={data.img}
+          alt=""/> 
+          <h3>{d.title}</h3>
+        </div>
+      ))}
+          </div>
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const sweetPancakes = [
+    {
+        id: 1,
+        title: "Chocolate Pancake",
+        img: "https://www.thespruceeats.com/thmb/gdkMYYmR-d4T-Sepmzxq7Dy7CcY=/2696x2696/smart/filters:no_upscale()/peppermint-hot-chocolate-pancakes-4159481-hero-01-0ff1a033b1c74c1bbd8c4c191156df1c.jpg"
+    },
+    {
+        id: 2,
+        title: "Sugar Pancake",
+        img: "https://www.thespruceeats.com/thmb/gdkMYYmR-d4T-Sepmzxq7Dy7CcY=/2696x2696/smart/filters:no_upscale()/peppermint-hot-chocolate-pancakes-4159481-hero-01-0ff1a033b1c74c1bbd8c4c191156df1c.jpg"
+    },
+    {
+        id: 3,
+        title: "Syrup Pancake",
+        img: "https://www.thespruceeats.com/thmb/gdkMYYmR-d4T-Sepmzxq7Dy7CcY=/2696x2696/smart/filters:no_upscale()/peppermint-hot-chocolate-pancakes-4159481-hero-01-0ff1a033b1c74c1bbd8c4c191156df1c.jpg"
+    },
+    {
+        id: 4,
+        title: "Ice cream Pancake",
+        img: "https://www.thespruceeats.com/thmb/gdkMYYmR-d4T-Sepmzxq7Dy7CcY=/2696x2696/smart/filters:no_upscale()/peppermint-hot-chocolate-pancakes-4159481-hero-01-0ff1a033b1c74c1bbd8c4c191156df1c.jpg"
+    }
+];
+
+export const savoryPancakes = [
+    {
+        id: 1,
+        title: "Ham Pancake",
+        img: "https://www.thespruceeats.com/thmb/gdkMYYmR-d4T-Sepmzxq7Dy7CcY=/2696x2696/smart/filters:no_upscale()/peppermint-hot-chocolate-pancakes-4159481-hero-01-0ff1a033b1c74c1bbd8c4c191156df1c.jpg"
+    },
+    {
+        id: 2,
+        title: "Veggie Pancake",
+        img: "https://www.thespruceeats.com/thmb/gdkMYYmR-d4T-Sepmzxq7Dy7CcY=/2696x2696/smart/filters:no_upscale()/peppermint-hot-chocolate-pancakes-4159481-hero-01-0ff1a033b1c74c1bbd8c4c191156df1c.jpg"
+    }
+];
+export const experimentalPancakes = [
+    {
+        id: 1,
+        title: "Chilli flakes Pancake",
+        img: "https://www.thespruceeats.com/thmb/gdkMYYmR-d4T-Sepmzxq7Dy7CcY=/2696x2696/smart/filters:no_upscale()/peppermint-hot-chocolate-pancakes-4159481-hero-01-0ff1a033b1c74c1bbd8c4c191156df1c.jpg"
+    },
+    {
+        id: 2,
+        title: "milk Pancake",
+        img: "https://www.thespruceeats.com/thmb/gdkMYYmR-d4T-Sepmzxq7Dy7CcY=/2696x2696/smart/filters:no_upscale()/peppermint-hot-chocolate-pancakes-4159481-hero-01-0ff1a033b1c74c1bbd8c4c191156df1c.jpg"
+    }
+];
