a6947ec8c1c4cce97df2750895f2915f4bbf6442
Digidolli <iamsadekaye@gmail.com>
2022-03-01 14:31:39 +0000

    Added footer.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <footer>
+        This project was coded by <a href="https://www.instagram.com/digidolli/" target="_blank" rel="noreferrer">digidolli</a> and is <a href="https://github.com/digidolli/react-8-ball-app" target="_blank" rel="noreferrer"> open-sourced on Github </a> 
+      </footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
